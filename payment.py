4204c83b288ec16830e670198fb5d79dea945cf3
wini83 <mariusz.wincior@gmail.com>
2021-02-13 09:46:31 +0100

    it's works

--- payment.py
+++ payment.py
@@ -2,6 +2,8 @@ import datetime
 
 
 class Payment:
+    amount: float
+
     def __init__(self, payed=False, due_date=datetime.date(2021, 2, 28), amount=0.0, excel_row=0):
         self.due_date = due_date
         self.amount = amount
@@ -10,3 +12,6 @@ class Payment:
 
     def __str__(self):
         return f"{self.amount}zł - {self.payed} - {self.due_date}"
+
+    def is_near(self):
+        pass
--- payment_category.py
+++ payment_category.py
@@ -1,4 +1,10 @@
+from payment import Payment
+
+
 class PaymentCategory:
+    name: str
+    payments: list[Payment]
+
     def __init__(self, name="", column="C"):
         self.name = name
         self.payments = []
