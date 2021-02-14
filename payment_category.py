57d6acce9eb9b760f00f1c6959283beafcaf31db
wini83 <mariusz.wincior@gmail.com>
2021-02-14 19:39:32 +0100

    dsd

--- payment_category.py
+++ payment_category.py
@@ -3,11 +3,10 @@ from payment import Payment
 
 class PaymentCategory:
     name: str = None
-    payments: list[Payment] = None
 
     def __init__(self, name="", column="C"):
         self.name = name
-        self.payments = [Payment]
+        self.payments = list[Payment]
         self.column = column
 
     def __str__(self):
