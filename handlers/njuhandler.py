8d70e52024c8817be384b5c9b90a3f160d4cf941
wini83 <mariusz.wincior@gmail.com>
2023-12-27 12:13:50 +0100

    nju update excel

--- handlers/njuhandler.py
+++ handlers/njuhandler.py
@@ -1,3 +1,6 @@
+from datetime import date
+from datetime import datetime
+
 from loguru import logger
 
 from context import HandlerContext
@@ -26,6 +29,24 @@ class NjuHandler(AbstractHandler):
                 log_str += f"phone:{account['client'].phone_nmb} - "
                 log_str += print_summary(account["invoices_payable"], text_if_none="no unpaid invoices")
                 log_str += "}"
+                if not self.without_update:
+                    if len(account["invoices_current"])>0:
+                        total:float = 0.0
+                        paid:bool = True
+                        due_date = datetime.combine(account["invoices_current"][0].due_date, datetime.min.time())
+                        for invoice in account["invoices_current"]:
+                            total+=invoice.total()
+                            if not invoice.status_bool():
+                                paid = False
+                        context.payment_book.update_current_payment(
+                            sheet_name=account["sheet"],
+                            category_name=account["category"],
+                            amount=total,
+                            paid=paid,
+                            due_date=due_date,
+                            force_unpaid=True
+                                                                    )
+
 
             logger.info(log_str)
             context.statuses.append(log_str)
